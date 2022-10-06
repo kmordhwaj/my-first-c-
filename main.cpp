fb88d2ee827c2c27522f9987a1524059024f72bc
K Mordhwaj <kmordhwaz@gmail.com>
2022-10-06 20:51:49 +0530

    second commit

--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main () {
-    cout << "Hello World" << endl;
+    cout << "Hello World" <<endl;
+
+    int a = 30;
+    cout << a <<endl ;
+
+    char b = 'm';
+    cout << b <<endl;
+
+    string c = "mordhwaj";
+    cout << c <<endl;
+
+    bool d = true; // or 1
+    cout << d <<endl;
+
+    bool e = 0; //or false
+    cout << e <<endl;
+
+    float f = 3.14;
+    cout << f <<endl;
+
+    double g = 1.3567;
+    cout << g <<endl;
+
+    int h = sizeof(g); //its showing bit
+    cout << "size of d is: " << h <<endl;
 }
